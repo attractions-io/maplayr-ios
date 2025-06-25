ad3781bc75f7f655fa2a517caa1c09fdd6ef4914
Robert Pugh <aquilosion@icloud.com>
2025-06-25 11:44:41 +0100

    Update MapLayr to v0.14

--- Package.swift
+++ Package.swift
@@ -32,8 +32,8 @@ let package = Package(
 		),
 		.binaryTarget(
 			name: "MapLayrInternal",
-			url: "https://cdn.attractions.io/frameworks/maplayr-ios/v0.13/MapLayrInternal.xcframework.zip",
-			checksum: "f30bff1bc7e2542f2e18eb4e56fcd1e2fd869fc832c05a404ddd0236ae2956c0"
+			url: "https://cdn.attractions.io/frameworks/maplayr-ios/v0.14/MapLayrInternal.xcframework.zip",
+			checksum: "ed79d06e6d6d4ab9cb5be661e0fbfdc2e0e2ff13a5696fdc0a83b3c7adf43b89"
 		),
 		.binaryTarget(
 			name: "MapGeometry",
